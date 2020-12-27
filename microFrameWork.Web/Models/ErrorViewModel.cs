e214d4b41258d9f4fb7d8ad6f2d44d0169898100
Emerson palacio <emersonpalaciootalvaro@gmail.com>
2020-12-27 07:03:04 -0500

    initial

---
+++ microFrameWork.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace microFrameWork.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
